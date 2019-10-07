249beabfdc48488628bcc3bb01e72fad7c216136
RokhayaN <rokhayamuslima1@gmail.com>
2019-10-07 19:19:59 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,7 +3,7 @@ def introduction ("rokhaya")
   puts "Hi, my name is #{rokhaya}."
 end
 
-def introduction ("rokhaya" , "ruby")
+def introduction ("rokhaya" , ruby)
   
   puts "Hi , my name is #{name} and i m a #{language} student"
 end
